9018d1fcb9b063c3d1820938c1e6a60942e66ae7
Dobes Vandermeer <dobesv@gmail.com>
2017-02-10 14:18:57 -0800

    Make sure bash_completions is loaded

--- bashrc.sh
+++ bashrc.sh
@@ -4,3 +4,8 @@ for MODULE in prompt.sh path.sh env.sh ; do
     . $HOME/.bash/bashrc/$MODULE
 done
 
+# Load completion scripts
+if [ -f /etc/bash_completion ] && ! shopt -oq posix; then
+    . /etc/bash_completion
+fi
+
